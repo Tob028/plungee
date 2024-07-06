b075f3d7292358a85213b601d55cb770bce6a10d
Tobias Bednar <me@tobb.dev>
2024-07-06 09:17:34 +0200

    Implement WatchConnectivity

---
+++ Shared/Utils/ExposureType.swift
@@ -0,0 +1,20 @@
+//
+//  ExposureType.swift
+//  plungee
+//
+//  Created by Tobias on 15/01/2024.
+//
+
+import Foundation
+
+enum ExposureType: String {
+    case plunge = "🧊 Plunge"
+    case shower = "🛀🏻 Shower"
+    case sauna = "😶‍🌫️ Sauna"
+}
+
+var exposureTypes: [ExposureType] = [.plunge, .shower, .sauna]
+
+extension ExposureType: Identifiable {
+    var id: String { rawValue }
+}

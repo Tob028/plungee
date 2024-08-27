38814fddcdbf3d0e7a049415e12e33c872528a9d
Tobias Bednar <me@tobb.dev>
2024-08-27 17:16:58 +0200

    Initial Commit

---
+++ plungee/plungeeApp.swift
@@ -0,0 +1,17 @@
+//
+//  plungeeApp.swift
+//  plungee
+//
+//  Created by Tobias on 27/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct plungeeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
